d5c493572b034ed42baa72feb7647ace5923a043
Irwing Naranjo <khalisser@gmail.com>
2024-12-18 20:22:38 -0300

    fix: update Telescope LogWatcher configuration to include multiple log levels

--- config/telescope.php
+++ config/telescope.php
@@ -170,7 +170,7 @@ return [
 
         Watchers\LogWatcher::class => [
             'enabled' => env('TELESCOPE_LOG_WATCHER', true),
-            'level' => 'error',
+            'level' => 'error, debug, info, notice, warning, alert, critical, emergency',
         ],
 
         Watchers\MailWatcher::class => env('TELESCOPE_MAIL_WATCHER', true),
